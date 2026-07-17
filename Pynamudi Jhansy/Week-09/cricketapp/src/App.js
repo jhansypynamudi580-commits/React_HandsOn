6dd161105aa88f9d8bb65feca5a9e0607ebf0d62
jhansypynamudi580 <jhansypynamudi580Qgmail.com>
2026-07-17 11:04:54 +0530

    React_HandsOn

---
+++ Pynamudi Jhansy/Week-09/cricketapp/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ color: "red" }}>THIS IS WEEK-10</h1>
+    </div>
+  );
+}
+
+export default App;
